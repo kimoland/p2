ffc540b623657d5c7a71fb0add14575d1aaa0c32
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-23 08:50:17 -0700

    Update api2.php

--- phpinfo/api2.php
+++ phpinfo/api2.php
@@ -1,1 +1,14 @@
+<?php
 
+$prox=json_decode(file_get_contents("https://dl.dropboxusercontent.com/s/dn0mdwhk7mjwa0k/novingram_server.json"),true);
+for ($i = 0; $i < 8;$i++)
+{
+    $show = $prox["login"][$i];
+    $message .= "🌐Proxy: "."$show"."\n\n";
+    
+}
+  
+
+
+?>
+    <?php echo $message; ?>
