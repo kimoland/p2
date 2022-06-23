7da19f8bc98fd54ffaa34ccc192fef3cca9ec717
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-23 08:29:07 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -33,15 +33,6 @@ $lastname = $message->from->last_name;
 $username = $message->from->username;
 $proxy = file_get_contents("https://proxykn7.herokuapp.com/proxy");
 $admin = 710732845;
-for ($i = 0; $i < 5;$i++)
-{
-	$prox=json_decode(file_get_contents("https://mtpro.xyz/api/?type=mtproto"),true);
-	$host=$prox['$i']['host'];
-	$port=$prox['$i']['port'];
-	$secret=$prox['$i']['secret'];
-        $messagepr .= "🌐Proxy: "."https://t.me/proxy?server=$host&port=$port&secret=$secret"."\n\n";
-    
-}
 $message_id2 = $message->message_id;
 
 //==================================
