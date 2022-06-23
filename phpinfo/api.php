040092c8677e6c6bebc527fcc51a1d839843af09
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-23 08:31:10 -0700

    Update api.php

--- phpinfo/api.php
+++ phpinfo/api.php
@@ -1,14 +1,13 @@
 <?php
 
 $prox=json_decode(file_get_contents("https://mtpro.xyz/api/?type=mtproto"),true);
-for ($i = 0; $i < 5;$i++)
-{
+
 	$host=$prox['$i']['host'];
 	$port=$prox['$i']['port'];
 	$secret=$prox['$i']['secret'];
     $messagepr .= "🌐Proxy: "."https://t.me/proxy?server=$host&port=$port&secret=$secret"."\n\n";
     
-}
+
 
 ?>
     <?php echo $messagepr; ?>
