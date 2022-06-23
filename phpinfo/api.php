399bd58008ba5d3650eaefe1aacbd8ee63d44a07
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-23 08:29:45 -0700

    Update api.php

--- phpinfo/api.php
+++ phpinfo/api.php
@@ -1,31 +1,15 @@
 <?php
 
-$str = file_get_contents("https://dl.dropboxusercontent.com/s/dn0mdwhk7mjwa0k/novingram_server.json");
-$enc = json_decode($str,true)["login"];
-$key = substr(hash("sha256",substr($enc,strlen($enc) - 16)),0,32);
-
-
-$str = substr($enc,0,strlen($enc) - 16);
-
-$dec = decrypt(trim($str),trim($key));
-$dec = substr($dec,16,strlen($dec));
-
-
-$json = json_decode($dec,true);
-for ($i = 0; $i < count($json);$i++)
+$prox=json_decode(file_get_contents("https://mtpro.xyz/api/?type=mtproto"),true);
+for ($i = 0; $i < 5;$i++)
 {
-    $ip = $json[$i]["ip"];
-    $port = $json[$i]["prt"];
-    $secret = $json[$i]["secret"];
-    $message .= "<a href='" . "https://t.me/proxy?server=$ip&port=$port&secret=$secret" . "'>"."https://t.me/proxy?server=$ip&port=$port&secret=$secret"."</a>" .  "<br>";
+	$host=$prox['$i']['host'];
+	$port=$prox['$i']['port'];
+	$secret=$prox['$i']['secret'];
+    $messagepr .= "🌐Proxy: "."https://t.me/proxy?server=$host&port=$port&secret=$secret"."\n\n";
+    
 }
 
 ?>
+    <?php echo $messagepr; ?>
 
-<html>
-  <head>      
-  </head>
-  <body>
-    <?php echo $message; ?>
-  </body>
-</html>
