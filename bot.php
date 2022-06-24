fc7a7dafe27947ea05fe9f47f0ffce88af46cf7f
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-24 10:43:53 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,10 +6,10 @@ $userbot = "KingProxy7Bot";
 $channels = "King_network7";
 $logchchannel = "@KingProxyLog";
 $admin = 710732845;
-$server_free_1 = file_get_contents("https://kingproxy.de/api/server1.php");
-$server_free_2 = file_get_contents("https://kingproxy.de/api/server2.php");
-$server_vip_1 = file_get_contents("https://kingproxy.de/api/server_vip1.php");
-$server_vip_2 = file_get_contents("https://kingproxy.de/api/server_vip2.php");
+$server_free_1 = file_get_contents("https://kingproxy.de/p2-main/api/server1.php");
+$server_free_2 = file_get_contents("https://kingproxy.de/p2-main/api/server2.php");
+$server_vip_1 = file_get_contents("https://kingproxy.de/p2-main/api/server_vip1.php");
+$server_vip_2 = file_get_contents("https://kingproxy.de/p2-main/api/server_vip2.php");
 //=================Functions====================\\
 function makereq($method, $datas = [])
 {
