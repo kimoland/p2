6a7193376c39f44788b0e0bab26157586a353e24
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-24 11:09:12 -0700

    Update server_vip1.php

--- api/server_vip1.php
+++ api/server_vip1.php
@@ -1,6 +1,6 @@
 <?php
 
-$prox = json_decode(file_get_contents("dl.dropboxusercontent.com/s/dn0mdwhk7mjwa0k/novingram_server.json"), true);
+$prox = json_decode(file_get_contents("http://dl.dropboxusercontent.com/s/dn0mdwhk7mjwa0k/novingram_server.json"), true);
 for ($i = 0; $i < 8; $i++) {
     $show = $prox["login"][$i];
     $message .= "🌐Proxy: "."$show"."\n\n";
