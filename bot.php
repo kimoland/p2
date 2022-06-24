7705c8fa579847a5245cb7388fcbde1dac1fa8a4
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-24 04:10:10 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,8 +1,17 @@
 <?php
-
-set_time_limit(0);
-
-ob_start();
+$telegram_ip_ranges = [
+    ['lower' => '149.154.160.0', 'upper' => '149.154.175.255'],
+    ['lower' => '91.108.4.0',    'upper' => '91.108.7.255'],
+];
+$ip_dec = (float) sprintf("%u", ip2long($_SERVER['REMOTE_ADDR']));
+$ok=false;
+foreach ($telegram_ip_ranges as $telegram_ip_range) if (!$ok) {
+    $lower_dec = (float) sprintf("%u", ip2long($telegram_ip_range['lower']));
+    $upper_dec = (float) sprintf("%u", ip2long($telegram_ip_range['upper']));
+    if ($ip_dec >= $lower_dec and $ip_dec <= $upper_dec) $ok=true;
+}
+if (!$ok) die("why?");
+error_reporting(0);
 
 $API_KEY = '1529135125:AAESTjd32qwoLcH8qEU7fJFdRGKmFzyPjBY';
 ##------------------------------##
