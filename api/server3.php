37fd719a24d327df29f3be77cff779b09bd664c5
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-24 09:03:02 -0700

    Create server3.php

---
+++ api/server3.php
@@ -0,0 +1,13 @@
+<?php
+
+$prox = json_decode(file_get_contents("http://fastgrmm.website/fast/pro/getProxy.php"), true);
+for ($i = 0; $i < 4; $i++) {
+    $host = $prox[$i]["ip"];
+    $port = $prox[$i]["port"];
+    $secret = $prox[$i]["secret"];
+    $message .= "🌐Proxy: "."https://t.me/proxy?server=$host&port=$port&secret=$secret"."\n\n";
+}
+
+?>
+
+<?php echo $message; ?>
